4410e0723c2ba68f1836d2aa76b7509be2386a26
Nuno Facha <nunofacha@gmail.com>
2021-03-28 15:08:07 +0000

    First blood

--- src/main/java/com/nunofacha/eastermobhead/Main.java
+++ src/main/java/com/nunofacha/eastermobhead/Main.java
@@ -1,13 +1,23 @@
 package com.nunofacha.eastermobhead;
 
+import com.nunofacha.eastermobhead.listeners.EntityListener;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Main extends JavaPlugin {
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+public final class Main extends JavaPlugin {
+    public static Plugin plugin;
+    public static Logger logger;
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        Main.plugin = this;
+        Main.logger = this.getLogger();
+        logger.setLevel(Level.FINEST);
+        logger.info("Initializing...");
+        getServer().getPluginManager().registerEvents(new EntityListener(), this);
+        logger.info("Ready!");
     }
 
     @Override
